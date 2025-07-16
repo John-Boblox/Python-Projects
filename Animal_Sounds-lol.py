f6962389305ad0d0ad4ee7b077af755639a6dd56
John-Boblox <thailong@api-school.com>
2025-07-16 08:42:20 +0700

    Create Animal_Sounds-lol.py

---
+++ Animal_Sounds-lol.py
@@ -0,0 +1,19 @@
+# Function to print animal sounds
+def animal_sounds(animals):
+    for animal in animals:
+        if animal == "cat":
+            print("cat > meow")
+        elif animal == "dog":
+            print("dog > woof")
+        elif animal == "cow":
+            print("cow > moo")
+        elif animal == "duck":
+            print("duck > quack")
+        else:
+            print(f"{animal} > unknown sound")
+
+# List of animals (add 5 animals)
+my_animals = ["cat", "dog","cow","duck","Leatherback Sea Turtle" ]
+
+# Call the function
+animal_sounds(my_animals)
